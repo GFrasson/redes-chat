808d31af970058ebc2875e4a88c995d84ce08939
Gabriel Frasson <bielfc27@gmail.com>
2023-04-25 00:27:12 -0300

    Define route from router

--- server/server_arithmetic.py
+++ server/server_arithmetic.py
@@ -5,6 +5,6 @@ from use_cases.ArithmeticUseCase import ArithmeticUseCase
 if __name__ == '__main__':
     server = UDPServer()
 
-    server.on('/arithmetic', ArithmeticUseCase().execute)
+    server.router.on('/arithmetic', ArithmeticUseCase().execute)
     
     server.listen()
